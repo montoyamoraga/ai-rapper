f6f03b7e80b950b5d36baa8a3ce6512937c339de
montoyamoraga <montoyamoraga@gmail.com>
2018-08-30 00:27:36 +0200

    add python script with voices for lil wayne and kendrick lamar

--- script.py
+++ script.py
@@ -2,6 +2,51 @@
 import mido
 import time
 import rtmidi
+import os
+
+# fileLil = open("lil-wayne.txt", "r")
+# linesLil = fileLil.read().split("\n")
+
+# content = open("lil-wayne.txt").readlines()
+
+textLil = [line.rstrip("\n") for line in open('lil-wayne.txt')]
+
+textKendrick = [line.rstrip("\n") for line in open('kendrick-lamar.txt')]
+
+voiceLil = " -v Alex "
+voiceKendrick = " -v Fred "
+
+minLength = min(len(textKendrick), len(textLil))
+
+counter = 0
+
+while True:
+    lineLil = textLil[counter]
+    os.system("say " + voiceLil + lineLil)
+    time.sleep(1.0)
+
+    lineKendrick = textKendrick[counter]
+    os.system("say " + voiceKendrick + lineKendrick)
+    time.sleep(1.0)
+
+    counter = counter + 1
+    counter = counter % minLength
+
+
+
+# for line in ltextLil:
+#     os.system("say " + line)
+#     time.sleep(1.0)
+
+# print(textLil)
+#
+# with open("lil-wayne.txt") as f:
+#     lines = f.readlines()
+
+
+# print(content)
+
+# os.system("say 'hi moises 404'")
 
 midiOut = rtmidi.MidiOut()
 
